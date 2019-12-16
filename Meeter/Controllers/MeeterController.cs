6a7ae72c30bba737cabbda0212f79ce8e952142b
Kamil Górzyński <kamil.gorzynski8@gmail.com>
2019-12-17 00:01:50 +0100

    MeeterController

--- Meeter/Controllers/MeeterController.cs
+++ Meeter/Controllers/MeeterController.cs
@@ -8,35 +8,35 @@ namespace Meeter.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class MeeterController : ControllerBase
     {
-        // GET api/values
+        // GET api/meeter
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/meeter/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/meeter
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/meeter/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/meeter/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
